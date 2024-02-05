df49df5e169b7ea54754fa83413fb1781c48e51e
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-05 16:03:43 +0100

    Enable ConfigurationProperties in LocationServiceApplication
    The @EnableConfigurationProperties annotation has been added to the LocationServiceApplication class. This change allows for the use of the LocationProperties class within this application, providing more configuration flexibility. The properties within LocationProperties can now be validated, updated and managed from within LocationServiceApplication.

--- location-service/src/main/java/com/sonalake/meetup/service/location/LocationServiceApplication.java
+++ location-service/src/main/java/com/sonalake/meetup/service/location/LocationServiceApplication.java
@@ -2,8 +2,10 @@ package com.sonalake.meetup.service.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LocationProperties.class)
 public class LocationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LocationServiceApplication.class, args);
