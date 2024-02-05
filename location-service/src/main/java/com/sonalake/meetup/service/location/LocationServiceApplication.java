a63d3833e55f5aa903e2f784c3e9495f6c7e1c7e
MatisL <ladislav.matis@broadridge.com>
2024-02-05 19:38:20 +0100

    Enable discovery client in LocationServiceApplication

--- location-service/src/main/java/com/sonalake/meetup/service/location/LocationServiceApplication.java
+++ location-service/src/main/java/com/sonalake/meetup/service/location/LocationServiceApplication.java
@@ -3,9 +3,11 @@ package com.sonalake.meetup.service.location;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LocationProperties.class)
+@EnableDiscoveryClient
 public class LocationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LocationServiceApplication.class, args);
