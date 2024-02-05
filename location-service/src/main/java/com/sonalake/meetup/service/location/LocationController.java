3318b88a8d8628db4c49f9c8bf936882b9fc6a2a
MatisL <ladislav.matis@broadridge.com>
2024-02-05 18:18:55 +0100

    Extend LocationController with new endpoint

--- location-service/src/main/java/com/sonalake/meetup/service/location/LocationController.java
+++ location-service/src/main/java/com/sonalake/meetup/service/location/LocationController.java
@@ -1,7 +1,21 @@
 package com.sonalake.meetup.service.location;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
+@RequestMapping("/location")
 public class LocationController {
+    private final LocationProperties locationProperties;
+
+    public LocationController(LocationProperties locationProperties) {
+        this.locationProperties = locationProperties;
+    }
+
+    @RequestMapping("/cities")
+    public Set<CityDto> getCities() {
+        return locationProperties.getCities();
+    }
 }
