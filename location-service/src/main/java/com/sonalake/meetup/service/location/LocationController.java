6c518fc0b14e179eba550eb9afee695821c5fd98
MatisL <ladislav.matis@broadridge.com>
2024-02-06 14:11:41 +0100

    Add logger to LocationController

--- location-service/src/main/java/com/sonalake/meetup/service/location/LocationController.java
+++ location-service/src/main/java/com/sonalake/meetup/service/location/LocationController.java
@@ -1,5 +1,7 @@
 package com.sonalake.meetup.service.location;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("/location")
 public class LocationController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
     private final LocationProperties locationProperties;
 
     public LocationController(LocationProperties locationProperties) {
@@ -16,6 +19,8 @@ public class LocationController {
 
     @RequestMapping("/cities")
     public Set<CityDto> getCities() {
+        LOGGER.info("Retrieving cities");
+
         return locationProperties.getCities();
     }
 }
