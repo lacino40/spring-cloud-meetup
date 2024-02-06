9a1f1ff3423e9a600f844d9eb83a55c39ecbb6f4
MatisL <ladislav.matis@broadridge.com>
2024-02-06 16:02:18 +0100

    Add zipkin

--- location-service/src/main/java/com/sonalake/meetup/service/location/LocationServiceApplication.java
+++ location-service/src/main/java/com/sonalake/meetup/service/location/LocationServiceApplication.java
@@ -1,9 +1,11 @@
 package com.sonalake.meetup.service.location;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LocationProperties.class)
@@ -11,5 +13,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class LocationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LocationServiceApplication.class, args);
+    }
+
+    @Bean
+    public Sampler getSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
