7604593f23c0a33d98bb1c2268208646fec2a1ab
MatisL <ladislav.matis@broadridge.com>
2024-02-06 17:35:38 +0100

    Remove unused logger from LocationController

--- location-service/src/main/java/com/sonalake/meetup/service/location/LocationController.java
+++ location-service/src/main/java/com/sonalake/meetup/service/location/LocationController.java
@@ -1,7 +1,5 @@
 package com.sonalake.meetup.service.location;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +8,6 @@ import java.util.Set;
 @RestController
 @RequestMapping("/location")
 public class LocationController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
     private final LocationProperties locationProperties;
 
     public LocationController(LocationProperties locationProperties) {
@@ -19,8 +16,6 @@ public class LocationController {
 
     @RequestMapping("/cities")
     public Set<CityDto> getCities() {
-        LOGGER.info("Retrieving cities");
-
         return locationProperties.getCities();
     }
 }
