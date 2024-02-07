25a766584edc46de6e04cf2c8917d8d46dd17432
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-07 14:40:02 +0100

    updated to include application names, always sample tracing, and activator features.

--- config-server/src/main/java/com/sonalake/meetup/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/sonalake/meetup/configserver/ConfigServerApplication.java
@@ -1,13 +1,20 @@
 package com.sonalake.meetup.configserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @Bean
+    public Sampler getSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
--- eureka-server/src/main/java/com/sonalake/meetup/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/sonalake/meetup/eureka/EurekaServerApplication.java
@@ -1,13 +1,20 @@
 package com.sonalake.meetup.eureka;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Bean
+    public Sampler getSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
