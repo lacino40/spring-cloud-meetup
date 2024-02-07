dd9c4dfeb866cae124540114ac8d588cddbd480a
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-07 17:21:13 +0100

    Update WebServiceApplication to include new beans

--- web-service/src/main/java/com/sonalake/meetup/service/web/WebServiceApplication.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/WebServiceApplication.java
@@ -1,12 +1,16 @@
 package com.sonalake.meetup.service.web;
 
 import brave.sampler.Sampler;
+import com.sonalake.meetup.service.web.impl.ForecastServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties(WebProperties.class)
 @EnableDiscoveryClient
 public class WebServiceApplication {
     public static void main(String[] args) {
@@ -16,5 +20,15 @@ public class WebServiceApplication {
     @Bean
     public Sampler getSampler() {
         return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ForecastService forecastService(RestTemplate restTemplate, WebProperties webProperties) {
+        return new ForecastServiceImpl(restTemplate, webProperties);
     }
 }
