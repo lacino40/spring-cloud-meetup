8f4026ca33cb4cb58e46379d08d3ff5237afaa1f
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-07 17:21:53 +0100

    Add @AllArgsConstructor and ForecastService to WebController

--- web-service/src/main/java/com/sonalake/meetup/service/web/WebController.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/WebController.java
@@ -1,14 +1,19 @@
 package com.sonalake.meetup.service.web;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class WebController {
+    private final ForecastService forecastService;
 
     @GetMapping("/forecast")
     public String getForecast(Model model) {
+        forecastService.addCitiesToModel(model);
+
         return "index";
     }
 }
