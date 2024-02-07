a0c7e85f41a4488c5990f04cae7e2d401b1b5c8d
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-07 14:40:26 +0100

    Update run.bat to remove Eureka service URL

--- zipkin-server/run.bat
+++ zipkin-server/run.bat
@@ -2,4 +2,6 @@
 SET "JAVA_HOME=c:\Program Files\Java\jdk-21.0.2\"
 SET "PATH=%JAVA_HOME%\bin;%PATH%"
 
-java -jar lib\zipkin-server-3.0.5-exec.jar --EUREKA_SERVICE_URL=http://localhost:8761/eureka/v2
+java -jar lib\zipkin-server-3.0.5-exec.jar
+REM EUREKA_SERVICE_URL defines the URL for Eureka service discovery
+REM --EUREKA_SERVICE_URL=http://localhost:8761/eureka/v2
