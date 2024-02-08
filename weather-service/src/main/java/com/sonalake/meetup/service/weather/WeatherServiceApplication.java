da1bb68917ff2de1a6fcc9329240219d4f4ac7f9
MatisL <ladislav.matis@broadridge.com>
2024-02-08 19:00:47 +0100

    Enhance WeatherServiceApplication with new beans

--- weather-service/src/main/java/com/sonalake/meetup/service/weather/WeatherServiceApplication.java
+++ weather-service/src/main/java/com/sonalake/meetup/service/weather/WeatherServiceApplication.java
@@ -1,13 +1,34 @@
 package com.sonalake.meetup.service.weather;
 
+import brave.sampler.Sampler;
+import com.sonalake.meetup.service.weather.impl.WeatherServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(WeatherProperties.class)
 public class WeatherServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeatherServiceApplication.class, args);
+    }
+
+    @Bean
+    public Sampler getSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WeatherService forecastService(RestTemplate restTemplate, WeatherProperties weatherProperties) {
+        return new WeatherServiceImpl(restTemplate, weatherProperties);
     }
 }
