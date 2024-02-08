28393b8a81a6fc59fa33cc4dfa7126cae42bff9d
MatisL <ladislav.matis@broadridge.com>
2024-02-08 16:03:09 +0100

    In this commit, we've made changes to simplify the `addCitiesToModel` method in the `ForecastServiceImpl.java` class. Previously, this function used a private helper method to prepare the city options set, but now it does so directly within itself. This change eliminates the need for the `getCitiesComboOptions` method, streamlining the code and improving readability.

--- web-service/src/main/java/com/sonalake/meetup/service/web/impl/ForecastServiceImpl.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/impl/ForecastServiceImpl.java
@@ -14,7 +14,6 @@ import java.net.URI;
 import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toSet;
 import static org.apache.commons.lang.StringUtils.isBlank;
 
 @AllArgsConstructor
@@ -30,9 +29,11 @@ public class ForecastServiceImpl extends ForecastUtility implements ForecastServ
 
     @Override
     public ForecastService addCitiesToModel(Model model) {
-        Set<ComboOption> citiesComboOptions = getCitiesComboOptions();
+        ResponseEntity<CityDto[]> response = restTemplate.getForEntity(CITIES_URI, CityDto[].class);
+        Set<ComboOption> citiesComboOptions = getCitiesComboOptions(requireNonNull(response.getBody()));
 
         model.addAttribute("cityOptions", citiesComboOptions);
+
         return this;
     }
 
@@ -49,13 +50,5 @@ public class ForecastServiceImpl extends ForecastUtility implements ForecastServ
     @Override
     public String template(String templateName) {
         return templateName;
-    }
-
-    private Set<ComboOption> getCitiesComboOptions() {
-        ResponseEntity<CityDto[]> response = restTemplate.getForEntity(CITIES_URI, CityDto[].class);
-
-        return sortCitiesById(requireNonNull(response.getBody()))
-                .map(this::toComboOption)
-                .collect(toSet());
     }
 }
