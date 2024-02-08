f0986431680aef2255d810956f20e577f091cd99
MatisL <ladislav.matis@broadridge.com>
2024-02-08 19:39:49 +0100

    Update weather service with OpenWeather API integration

--- weather-service/src/main/java/com/sonalake/meetup/service/weather/WeatherProperties.java
+++ weather-service/src/main/java/com/sonalake/meetup/service/weather/WeatherProperties.java
@@ -2,12 +2,26 @@ package com.sonalake.meetup.service.weather;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Data
-@ConfigurationProperties("service.weather.open-weather-map")
+@ConfigurationProperties("service.weather.open-weather")
 public class WeatherProperties {
     private String url;
     private String version;
     private String unit;
     private String apiKey;
+
+    public URI getOpenWeatherUrl(String query) {
+        return UriComponentsBuilder
+                .fromHttpUrl(url)
+                .pathSegment(version, "weather")
+                .queryParam("q", query)
+                .queryParam("units", unit)
+                .queryParam("APPID", apiKey)
+                .build()
+                .toUri();
+    }
 }
