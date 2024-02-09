8c8e3195c677e99ff83c9f20a1823b91c6111e00
MatisL <ladislav.matis@broadridge.com>
2024-02-09 14:05:22 +0100

    Add exception handling and implement file path capability in WeatherService

--- weather-service/src/main/java/com/sonalake/meetup/service/weather/impl/WeatherServiceImpl.java
+++ weather-service/src/main/java/com/sonalake/meetup/service/weather/impl/WeatherServiceImpl.java
@@ -1,22 +1,28 @@
 package com.sonalake.meetup.service.weather.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sonalake.meetup.service.weather.WeatherProperties;
 import com.sonalake.meetup.service.weather.WeatherService;
 import com.sonalake.meetup.service.weather.dto.OpenWeatherDto;
 import lombok.AllArgsConstructor;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Paths;
 
 @AllArgsConstructor
 public class WeatherServiceImpl implements WeatherService {
     private final RestTemplate restTemplate;
     private final WeatherProperties weatherProperties;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public OpenWeatherDto getWeather(String query) {
-        URI openWeatherUrl = weatherProperties.getOpenWeatherUrl(query);
+    public OpenWeatherDto getWeather(String query) throws IOException {
+        URI openWeatherURI = weatherProperties.getOpenWeatherUrl(query);
 
-        return restTemplate.getForObject(openWeatherUrl, OpenWeatherDto.class);
+        return weatherProperties.isMocked() ?
+                objectMapper.readValue(Paths.get(openWeatherURI).toFile(), OpenWeatherDto.class) :
+                restTemplate.getForObject(openWeatherURI, OpenWeatherDto.class);
     }
 }
