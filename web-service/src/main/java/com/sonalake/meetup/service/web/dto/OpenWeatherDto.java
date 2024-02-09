e22fc6f06d5161a7ea581296408913cedd148d46
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-09 18:51:32 +0100

    Refactor OpenWeatherDto to improve data representation.
    The class OpenWeatherDto has been simplified by removing a list of weather instances to a single instance to better represent weather data. The data types in the 'Main' and 'Wind' classes were changed from String to Double to better suit their data. Improves readability and data coherence.

--- web-service/src/main/java/com/sonalake/meetup/service/web/dto/OpenWeatherDto.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/dto/OpenWeatherDto.java
@@ -2,40 +2,31 @@ package com.sonalake.meetup.service.web.dto;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class OpenWeatherDto {
-    private List<Weather> weather;
+    private Weather displayWeather;
     private Main main;
     private Wind wind;
     private String name;
     private String visibility;
 
     @Data
-    public static class Weather {
-        private Long id;
+    private static class Weather {
         private String main;
         private String description;
         private String icon;
     }
 
     @Data
-    private static class Main {
-        private String temp;
-        private String feels_like;
+    public static class Main {
+        private Double temp;
+        private Double feels_like;
         private String pressure;
         private String humidity;
     }
 
     @Data
-    private static class Wind {
-        private String speed;
-    }
-
-    public Weather getDisplayWeather() {
-        return  weather.stream()
-                .findFirst()
-                .orElse(null);
+    public static class Wind {
+        private Double speed;
     }
 }
