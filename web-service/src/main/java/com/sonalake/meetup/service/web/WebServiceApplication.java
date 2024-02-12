4bf74a1c5366eb2914289c87fec03ecac1e523f7
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-12 12:11:43 +0100

    Refactor WebServiceApplication to use FeignClients

--- web-service/src/main/java/com/sonalake/meetup/service/web/WebServiceApplication.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/WebServiceApplication.java
@@ -1,17 +1,15 @@
 package com.sonalake.meetup.service.web;
 
 import brave.sampler.Sampler;
-import com.sonalake.meetup.service.web.impl.ForecastServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigurationProperties(WebProperties.class)
+@EnableFeignClients
 public class WebServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebServiceApplication.class, args);
@@ -20,15 +18,5 @@ public class WebServiceApplication {
     @Bean
     public Sampler getSampler() {
         return Sampler.ALWAYS_SAMPLE;
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public ForecastService forecastService(RestTemplate restTemplate, WebProperties webProperties) {
-        return new ForecastServiceImpl(restTemplate, webProperties);
     }
 }
