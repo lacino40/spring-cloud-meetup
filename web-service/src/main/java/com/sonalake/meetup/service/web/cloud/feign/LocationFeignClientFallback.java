e952c6367039cad06bf90c3be3301cd67ebc22ff
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-12 17:59:28 +0100

    initial commit

---
+++ web-service/src/main/java/com/sonalake/meetup/service/web/cloud/feign/LocationFeignClientFallback.java
@@ -0,0 +1,16 @@
+package com.sonalake.meetup.service.web.cloud.feign;
+
+import com.sonalake.meetup.service.web.dto.LocationDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LocationFeignClientFallback implements LocationFeignClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationFeignClientFallback.class);
+
+    @Override
+    public LocationDto[] getLocations() {
+        LOGGER.error("location-service not available");
+
+        return null;
+    }
+}
