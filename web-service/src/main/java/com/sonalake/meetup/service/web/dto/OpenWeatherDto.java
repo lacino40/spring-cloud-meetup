1bedeaec41c0e6ca3b8eb57532ee9f3d64dd36fd
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-13 11:35:47 +0100

    Enhance error handling in OpenWeatherDto class
    Modified the way error handling is carried out in the OpenWeatherDto class by including more detailed information about exceptions. This new structure expands the error details by capturing the service name that failed and the related stack trace. Also, boolean methods isError() and isOk() have been added to simplify error checking.

--- web-service/src/main/java/com/sonalake/meetup/service/web/dto/OpenWeatherDto.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/dto/OpenWeatherDto.java
@@ -1,28 +1,45 @@
 package com.sonalake.meetup.service.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import static org.apache.commons.lang.BooleanUtils.isFalse;
+import static org.apache.commons.lang.exception.ExceptionUtils.getStackTrace;
 
 @Data
 public class OpenWeatherDto {
     private Weather displayWeather;
     private Main main;
     private Wind wind;
+    private Error error;
     private String name;
     private String displayTime;
     private String visibility;
     private String iconUrl;
     private boolean mock;
-    private boolean error;
-
 
     /**
-     * Sets the error flag to true for the OpenWeatherDto object.
+     * Sets the error flag, error service name, and error stack trace when error occurred
+     * while calling remote service
      *
-     * @return The OpenWeatherDto object with the error flag set to true.
+     * @param errorServiceName the name of the error service
+     * @param cause the throwable object to get the stack trace from
+     * @return the OpenWeatherDto object
      */
-    public OpenWeatherDto flagError() {
-        this.error = true;
+    public OpenWeatherDto withThrowable(String errorServiceName, Throwable cause) {
+        setError(
+            Error.from(errorServiceName, getStackTrace(cause))
+        );
+
         return this;
+    }
+
+    public boolean isError() {
+        return getError().isError();
+    }
+
+    public boolean isOk() {
+        return isFalse(isError());
     }
 
     @Data
@@ -43,5 +60,13 @@ public class OpenWeatherDto {
     @Data
     public static class Wind {
         private Double speed;
+    }
+
+    @Data
+    @AllArgsConstructor(staticName = "from")
+    public static class Error {
+        private final boolean error = true;
+        private final String errorServiceName;
+        private final String errorStackTrace;
     }
 }
