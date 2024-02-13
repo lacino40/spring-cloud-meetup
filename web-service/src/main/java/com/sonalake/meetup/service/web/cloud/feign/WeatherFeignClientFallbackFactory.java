72dd02f9a6324f4b683b68b1f70707179a0c9f74
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-13 15:25:10 +0100

    Remove logging in WeatherFeignClientFallbackFactory

--- web-service/src/main/java/com/sonalake/meetup/service/web/cloud/feign/WeatherFeignClientFallbackFactory.java
+++ web-service/src/main/java/com/sonalake/meetup/service/web/cloud/feign/WeatherFeignClientFallbackFactory.java
@@ -1,16 +1,11 @@
 package com.sonalake.meetup.service.web.cloud.feign;
 
 import com.sonalake.meetup.service.web.dto.OpenWeatherDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FallbackFactory;
 
 public class WeatherFeignClientFallbackFactory implements FallbackFactory<WeatherFeignClient> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherFeignClientFallbackFactory.class);
     @Override
     public WeatherFeignClient create(Throwable cause) {
-        LOGGER.error("open-weather-api is not available");
-
         return query ->
                 new OpenWeatherDto()
                         .withThrowable(
