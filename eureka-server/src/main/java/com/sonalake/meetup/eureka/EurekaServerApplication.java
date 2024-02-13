5b494421634042ef39bef7c7d83c0f64ef9763cf
Ladislav Matis <ladislav.matis@321sd.sk>
2024-02-13 11:30:54 +0100

    Update Sampler logic in EurekaServerApplication
    This change adds a conditional sampling based on the 'zipkin.sampler.enabled' property value, rather than always using a constant value. This allows for more flexibility in trace sampling based on system configuration.

--- eureka-server/src/main/java/com/sonalake/meetup/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/sonalake/meetup/eureka/EurekaServerApplication.java
@@ -1,10 +1,13 @@
 package com.sonalake.meetup.eureka;
 
 import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
+
+import static java.lang.Boolean.TRUE;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -14,7 +17,12 @@ public class EurekaServerApplication {
     }
 
     @Bean
-    public Sampler getSampler() {
-        return Sampler.ALWAYS_SAMPLE;
+    public Sampler getSampler(@Value("zipkin.sampler.enabled") String enabled) {
+        return new Sampler() {
+            @Override
+            public boolean isSampled(long traceId) {
+                return TRUE.toString().equals(enabled);
+            }
+        };
     }
 }
